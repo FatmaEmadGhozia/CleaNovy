4703af024c6d1a7c0426049f5560171b4f74b33c
tasneem tawfeek <tasneemtawfeek410@gmail.com>
2026-06-15 17:31:00 +0300

    Remove duplicates

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,110 +1,18 @@
-
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-// import ProtectedRoute from "./ProtectedRoute";
-// import ForgotPassword from "../Pages/Auth/ForgotPassword";
-// import ResetPassword from "../Pages/Auth/ResetPassword";
-// import LandingPage from "../components/landing/LandingPage";
-// import LoginPage from "../Pages/Auth/LoginPage";
-// import SignUpPage from "../Pages/Auth/SignUpPage";
-// import ProviderLayout from "../Pages/provider/ProviderLayout";
-// import AboutPage from "../Pages/AboutPage/AboutPage";
-// import ContactPage from "../Pages/ContactPage/ContactPage";
-// import Settingspage from "../Pages/Settingspage/Settingspage";
-// import { CleannovyPage } from "../Pages/CleannovyPage";
-// import { Layout } from "../modules/app/components/Layout";
-// import { Dashboard } from "../modules/app/pages/Dashboard";
-// import { UsersManagement } from "../modules/app/pages/UsersManagement";
-// import { ProvidersManagement } from "../modules/app/pages/ProvidersManagement";
-// import { OrdersManagement } from "../modules/app/pages/OrdersManagement";
-// import { ServicesCategories } from "../modules/app/pages/ServicesCategories";
-// import { ReviewsReports } from "../modules/app/pages/ReviewsReports";
-
-
-// import CartPage from "../Pages/orders/CartPage"
-// import SchedulePage from "../Pages/orders/SchedulePage"
-// import CheckoutPage from "../Pages/orders/CheckoutPage"
-// export default function AppRoutes() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         {/*  Public Routes */}
-
-//         {/* orders routes */}
-
-//         <Route path="/cart" element={<CartPage />} />
-//         <Route path="/schedule" element={<SchedulePage />} />
-//         <Route path="/checkout" element={<CheckoutPage />} />
-
-//         {/* Public Routes */}
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/about" element={<AboutPage />} />
-//         <Route path="/contact" element={<ContactPage />} />
-//         <Route path="/forgot-password" element={<ForgotPassword />} />
-//         <Route path="/reset-password" element={<ResetPassword />} />
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/signup" element={<SignUpPage />} />
-
-//         {/* Provider Routes */}
-//         <Route path="/provider/*" element={<ProviderLayout />} />
-//         <Route
-//           path="/provider"
-//           element={<Navigate to="/provider/dashboard" replace />}
-//         />
-//         <Route path="/cleannovy" element={<CleannovyPage/>} />
-//         <Route path="/provider" element={<ProviderLayout />} />
-//         {/* <Route path="/provider/*" element={<ProviderLayout />} /> */}
-//         <Route path="/provider" element={<Navigate to="/provider/dashboard" replace />} />
-
-
-
-//         < Route path="/provider/*" element={
-//              <ProtectedProviderRoute>
-//                <ProviderLayout />
-//              </ProtectedProviderRoute>
-//                  } />
-//         {/* Protected Routes */}
-//         <Route
-//           path="/settings"
-//           element={
-//             <ProtectedRoute>
-//               <Settingspage />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route path="/" element={<Layout />}>
-//           <Route path="dashboard" element={<Dashboard />} />
-//           <Route path="users" element={<UsersManagement />} />
-//           <Route path="providers" element={<ProvidersManagement />} />
-//           <Route path="orders" element={<OrdersManagement />} />
-//           <Route path="services" element={<ServicesCategories />} />
-//           <Route path="reviews" element={<ReviewsReports />} />
-//         </Route>
-
-//         <Route path="*" element={<Navigate to="/" replace />} />
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
-
-
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import ProtectedRoute from "./ProtectedRoute";
 // import ProtectedProviderRoute from "./ProtectedProviderRoute";
 import ProtectedProviderRoute from "./ProtectedProviderRoute";
 
-import ForgotPassword from "../Pages/Auth/ForgotPassword";
-import ResetPassword from "../Pages/Auth/ResetPassword";
+import ForgotPassword from "../pages/Auth/ForgotPassword";
+import ResetPassword from "../pages/Auth/ResetPassword";
 import LandingPage from "../components/landing/LandingPage";
-import LoginPage from "../Pages/Auth/LoginPage";
-import SignUpPage from "../Pages/Auth/SignUpPage";
-import AboutPage from "../Pages/AboutPage/AboutPage";
-import ContactPage from "../Pages/ContactPage/ContactPage";
-import Settingspage from "../Pages/Settingspage/Settingspage";
-import { CleannovyPage } from "../Pages/CleannovyPage";
+import LoginPage from "../pages/Auth/LoginPage";
+import SignUpPage from "../pages/Auth/SignUpPage";
+import AboutPage from "../pages/AboutPage/AboutPage";
+import ContactPage from "../pages/ContactPage/ContactPage";
+import Settingspage from "../pages/Settingspage/Settingspage";
+import { CleannovyPage } from "../pages/CleannovyPage";
 import { Layout } from "@/modules/app/components/Layout";
 import { Dashboard } from "@/modules/app/pages/Dashboard";
 import { UsersManagement } from "@/modules/app/pages/UsersManagement";
@@ -114,24 +22,12 @@ import { ServicesCategories } from "@/modules/app/pages/ServicesCategories";
 import { ReviewsReports } from "@/modules/app/pages/ReviewsReports";
 
 
-import CartPage from "../Pages/orders/CartPage"
-import SchedulePage from "../Pages/orders/SchedulePage"
-import CheckoutPage from "../Pages/orders/CheckoutPage"
-import ShopPage from "../Pages/ShopPage/ShopPage"
+import CartPage from "../pages/orders/CartPage"
+import SchedulePage from "../pages/orders/SchedulePage"
+import CheckoutPage from "../pages/orders/CheckoutPage"
+import ShopPage from "../pages/ShopPage/ShopPage"
 
-import ProviderLayout from "../Pages/provider/ProviderLayout";
-
-import { Layout } from "../modules/app/components/Layout";
-import { Dashboard } from "../modules/app/pages/Dashboard";
-import { UsersManagement } from "../modules/app/pages/UsersManagement";
-import { ProvidersManagement } from "../modules/app/pages/ProvidersManagement";
-import { OrdersManagement } from "../modules/app/pages/OrdersManagement";
-import { ServicesCategories } from "../modules/app/pages/ServicesCategories";
-import { ReviewsReports } from "../modules/app/pages/ReviewsReports";
-
-import CartPage from "../Pages/orders/CartPage";
-import SchedulePage from "../Pages/orders/SchedulePage";
-import CheckoutPage from "../Pages/orders/CheckoutPage";
+import ProviderLayout from "../pages/provider/ProviderLayout";
 
 export default function AppRoutes() {
   return (
